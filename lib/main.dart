0eb417f559f3331dfa012618b4297b81e5b10dd3
LHjortlund <lbfe44184@edu.ucl.dk>
2025-02-11 10:51:06 +0100

    first

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 5, 79, 116),
         appBar: AppBar(
           title: Text('I Am Rich'),
           backgroundColor: const Color.fromARGB(255, 23, 138, 196),
